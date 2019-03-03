66a008d9acb7332e7500aff2f09f81c2c802d603
David Leuenberger <david.leuenberger@gmx.ch>
2019-03-03 14:40:56 +0100

    add gulpfile for start

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+
+gulp.task('start', function (done) {
+  nodemon({
+    script: 'dist/app.js'
+    , ext: 'js'
+    , env: { 'NODE_ENV': 'production' }
+    , done: done
+  })
+})
+
+gulp.task('default', 'start');
