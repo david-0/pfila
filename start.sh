cba728075550ae76e0637399365ed552c3bfb0f6
David Leuenberger <david.leuenberger@gmx.ch>
2019-03-04 17:55:44 +0100

    change start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd server
-nohup npm run start-prod > ../../log/server-$(date +%Y%m%d-%H%M%S).log 2> ../../log/server-error-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
+nohup npm start > ../log/server-$(date +%Y%m%d-%H%M%S).log 2> ../log/server-error-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
