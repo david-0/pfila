db154d591fe9ba16234d192055a65fc972d50b7c
David Leuenberger <david.leuenberger@gmx.ch>
2019-03-04 17:25:13 +0100

    change logfile path

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd server
-nohup npm run start-prod > ../../log/server.log 2> ../../log/server-error.log < /dev/zero &
+nohup npm run start-prod > ../../log/server-$(date +%Y%m%d-%H%M%S).log 2> ../../log/server-error-$(date +%Y%m%d-%H%M%S).log < /dev/zero &
